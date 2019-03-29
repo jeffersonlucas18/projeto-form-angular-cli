08baec975475dee40f2b15f53e2cd30e7f097c2a
jefferson lucas <jefferson.lucas114@gmail.com>
2019-03-28 22:33:05 -0300

    design da pagina

---
+++ src/app/clientes/shared/cliente.ts
@@ -0,0 +1,11 @@
+export class Cliente {
+    nome: string = '';
+    tipo : number = 1;
+    genero: number= 1;
+    datanascimento: Date = null;
+    observacao: string =  '';
+    inativo: boolean = false;
+    
+
+}
+
